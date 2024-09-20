ec526992cf306cc704f823c168f121df942b3023
guima <gabrielguimasr@gmail.com>
2024-09-20 15:40:57 -0300

    First CNN - 94%

--- torch/neural_network.py
+++ torch/neural_network.py
@@ -7,8 +7,10 @@ class MNIST_NN_Linear(nn.Module):
         self.layers = nn.Sequential(
             nn.Linear(784,100),
             nn.LeakyReLU(),
+            
             nn.Linear(100,50),
             nn.LeakyReLU(),
+            
             nn.Linear(50,10),
             nn.Softmax(dim=1)
         )
@@ -17,3 +19,33 @@ class MNIST_NN_Linear(nn.Module):
         X = X.view(X.shape[0], -1)
         X = self.layers(X)
         return X
+    
+
+class MNIST_CNN(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 6, 5, padding=1),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(2, stride=2),
+            
+            nn.Conv2d(6, 16, 5, padding=0),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(2, stride=2),
+            
+            nn.Flatten(),
+            nn.Linear(256, 100),
+            nn.LeakyReLU(),
+            
+            nn.Linear(100, 50),
+            nn.LeakyReLU(),
+            
+            nn.Linear(50, 10),
+            nn.Softmax(dim=1)
+        )
+        
+    def forward(self, X):
+        X = self.layers(X)
+        return X
+    
